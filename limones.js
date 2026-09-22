8e0963ea445c377fbe31ecdaaba4d8b356ee3340
Yessenia12 <quinatoalemayessenia@gmail.com>
2026-09-21 23:24:35 -0500

    Agregar Puntaje y Vidas

--- limones.js
+++ limones.js
@@ -10,6 +10,8 @@ let personajeX=canvas.width/2;
 let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=0;
+let puntaje=0;
+let vidas=3;
 
 function dibujarIniciar(){
     dibujarSuelo();
@@ -55,19 +57,26 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapada();
+    detectarPiso();
     
 }
-function detectarColision(){
+function detectarAtrapada(){
     if(limonX+ANCHO_LIMON>personajeX && limonX<personajeX+ANCHO_PERSONAJE && (limonY+ALTURA_LIMON>personajeY && limonY<personajeY+ALTURA_PERSONAJE)){
         aparecerLimon();
+        puntaje=puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
     }
 
 }
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
+function detectarPiso(){
+    if(limonY+ALTURA_LIMON==canvas.height-ALTURA_SUELO){
+         aparecerLimon();
+         vidas=vidas-1;
+        mostrarEnSpan("txtVidas",vidas);
+    }
 }
+
 function aparecerLimon(){
     limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
     limonY=0;
